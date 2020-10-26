26a72f7c9a144774d334c4b1ee7059eb2f13e214
Cbrito03 <63610450+Cbrito03@users.noreply.github.com>
2020-10-26 04:34:40 -0600

    Se modifico
    Se reemplazo el $CR por \n

--- controllers/msj_WA.js
+++ controllers/msj_WA.js
@@ -14,38 +14,38 @@ var colas = {
   }
 };
 
-var mensaje_df = "¡Hola! $cr Soy *Avi*, tu asistente virtual 🤖 de Claro $cr ";
-    mensaje_df +="¡Este es el nuevo menú de opciones con las que te puedo apoyar más rápido!  Solo envía una de las palabras que aparecen resaltadas según tu consulta. $cr $cr ";
-    mensaje_df +="➡️ Envía *cotizar* para conocer nuestros planes móviles y residenciales si deseas renovar o contratar nuevos servicios. 😎  😎 $cr $cr ";
-    mensaje_df +="➡️ Envía *pagar* para ver el saldo, fecha de vencimiento y pagar tu factura móvil y residencial. 💳 $cr $cr ";
-    mensaje_df +="➡️ Envía *recarga* para hacer una recarga.  $cr $cr ";
-    mensaje_df +="➡️ Envía *paquete* para compra de paquete. $cr $cr ";
-    mensaje_df +="➡️ Envía *ayuda* para conocer todo lo que puedes hacer en un mismo lugar. $cr $cr ";
-    mensaje_df +="➡️ Envía *asistencia* si necesitas ayuda con tus servicios móviles o fijos 📱 💻 📺. $cr $cr ";
-    mensaje_df +="➡️ Envía *Tienda* si deseas conocer los horarios de nuestras sucursales $cr $cr ";
-    mensaje_df +="➡️ Envía *club* para conocer los establecimientos con promociones especiales solo por ser cliente Claro. 😎 💰  $cr $cr ";
-    //mensaje_df +="➡️ Envía *asesor* si aún deseas ser atendido por uno de nuestros agentes de servicio al cliente o ventas. 👩💻👨💻 $cr $cr ";
+var mensaje_df = "¡Hola! \n Soy *Avi*, tu asistente virtual 🤖 de Claro \n ";
+    mensaje_df +="¡Este es el nuevo menú de opciones con las que te puedo apoyar más rápido!  Solo envía una de las palabras que aparecen resaltadas según tu consulta. \n \n ";
+    mensaje_df +="➡️ Envía *cotizar* para conocer nuestros planes móviles y residenciales si deseas renovar o contratar nuevos servicios. 😎  😎 \n \n ";
+    mensaje_df +="➡️ Envía *pagar* para ver el saldo, fecha de vencimiento y pagar tu factura móvil y residencial. 💳 \n \n ";
+    mensaje_df +="➡️ Envía *recarga* para hacer una recarga.  \n \n ";
+    mensaje_df +="➡️ Envía *paquete* para compra de paquete. \n \n ";
+    mensaje_df +="➡️ Envía *ayuda* para conocer todo lo que puedes hacer en un mismo lugar. \n \n ";
+    mensaje_df +="➡️ Envía *asistencia* si necesitas ayuda con tus servicios móviles o fijos 📱 💻 📺. \n \n ";
+    mensaje_df +="➡️ Envía *Tienda* si deseas conocer los horarios de nuestras sucursales \n \n ";
+    mensaje_df +="➡️ Envía *club* para conocer los establecimientos con promociones especiales solo por ser cliente Claro. 😎 💰  \n \n ";
+    //mensaje_df +="➡️ Envía *asesor* si aún deseas ser atendido por uno de nuestros agentes de servicio al cliente o ventas. 👩💻👨💻 \n \n ";
 
-var mjs_horario = '¡Hola, gracias por comunicarte a Claro, te informamos nuestros horarios de atención!  $cr $cr ';
-    mjs_horario += '⌚Facebook y Twitter  $cr $cr ';
-    mjs_horario += 'Lunes a Domingo  $cr $cr ';
-    mjs_horario += '6:00 am a 22:00 horas  $cr $cr $cr';
-    mjs_horario += 'Whatsapp disponible 24/7  $cr $cr $cr';
-    mjs_horario += 'Te invitamos a ingresar a https://miclaro.com.pa/pa/ disponible 24/7 para que puedas hacer tus autogestiones.  $cr $cr';
+var mjs_horario = '¡Hola, gracias por comunicarte a Claro, te informamos nuestros horarios de atención!  \n \n ';
+    mjs_horario += '⌚Facebook y Twitter  \n \n ';
+    mjs_horario += 'Lunes a Domingo  \n \n ';
+    mjs_horario += '6:00 am a 22:00 horas  \n \n \n';
+    mjs_horario += 'Whatsapp disponible 24/7  \n \n \n';
+    mjs_horario += 'Te invitamos a ingresar a https://miclaro.com.pa/pa/ disponible 24/7 para que puedas hacer tus autogestiones.  \n \n';
     mjs_horario += '¡Claro que sí!';
 
-var msj_ayuda = "Ingresa aquí 👉🏼  https://miclaro.com.pa/ si deseas realizar alguna de las siguientes gestiones: $cr $cr ";
-    msj_ayuda += "Pagos de factura $cr";
-    msj_ayuda += "Consulta de saldo $cr";
-    msj_ayuda += "Recargas $cr";
-    msj_ayuda += "Paquetes prepago  $cr";
-    msj_ayuda += "Horarios y ubicaciones de nuestras tiendas $cr";
-    msj_ayuda += "Promociones Vigentes  $cr";
+var msj_ayuda = "Ingresa aquí 👉🏼  https://miclaro.com.pa/ si deseas realizar alguna de las siguientes gestiones: \n \n ";
+    msj_ayuda += "Pagos de factura \n";
+    msj_ayuda += "Consulta de saldo \n";
+    msj_ayuda += "Recargas \n";
+    msj_ayuda += "Paquetes prepago  \n";
+    msj_ayuda += "Horarios y ubicaciones de nuestras tiendas \n";
+    msj_ayuda += "Promociones Vigentes  \n";
 
-var msj_club = "Si eres Claro 😉 eres parte del club con beneficios y descuentos. $cr $cr ";
-    msj_club += "¡Descarga la App! 👇 $cr $cr ";
-    msj_club += "Android: http://bit.ly/ClaroClub-Android $cr $cr ";
-    msj_club += "IOS: http://bit.ly/ClaroClubiOS $cr $cr ";
+var msj_club = "Si eres Claro 😉 eres parte del club con beneficios y descuentos. \n \n ";
+    msj_club += "¡Descarga la App! 👇 \n \n ";
+    msj_club += "Android: http://bit.ly/ClaroClub-Android \n \n ";
+    msj_club += "IOS: http://bit.ly/ClaroClubiOS \n \n ";
 
 var palabras = {
   "cotizar": {
@@ -70,7 +70,7 @@ var palabras = {
     "messages" : [
       {
         "type" : "text",
-        "text" :  "Te invitamos a conocer nuestros horarios de atención ingresando aquí 👉🏼 $cr https://www.claro.com.pa/personas/cacs/",
+        "text" :  "Te invitamos a conocer nuestros horarios de atención ingresando aquí 👉🏼 \n https://www.claro.com.pa/personas/cacs/",
         "mediaURL" : ""
       }
     ]
